236bc3eaca58e97a549fb05a5fc9e390a758dfbc
cephalin <cephalin@microsoft.com>
2016-03-17 13:16:04 -0500

    add files

---
+++ aspnet-get-started/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet_get_started.Startup))]
+namespace aspnet_get_started
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
